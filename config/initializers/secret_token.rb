ad0eb92516575316a81b2686a6269a795efbd359
robtwiley <robtwiley@cox.net>
2010-08-30 20:59:54 -0500

    Initial commite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab01PhotoAlbum::Application.config.secret_token = 'd356ae1fb61d185535179637d9b405b43761a2432b2292eec6cb9c3c9bba82670733f912da133cf585f1c08de464e88a73606e5f7145c50487e0f0c3fe5405d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lab01PhotoAlbum::Application.config.session_store :cookie_store, :key => '_Lab01_PhotoAlbum_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Lab01PhotoAlbum::Application.config.session_store :active_record_store
